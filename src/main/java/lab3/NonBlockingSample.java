1b3ce107dcc9fd11b5ee25ea6c1c2c813c18c1fd
Makrushyn <a.makrushyn@levi9.com>
2022-01-26 20:52:18 +0200

    [Lab3] Change array size

--- src/main/java/lab3/NonBlockingSample.java
+++ src/main/java/lab3/NonBlockingSample.java
@@ -8,15 +8,17 @@ import utils.ArrayUtils;
 
 public class NonBlockingSample {
 
+    private static final int DEFAULT_ARRAY_SIZE = 90000;
+
     public static void main(String[] args) throws InterruptedException {
-        long[] numbers = ArrayUtils.getRandomArrayLong(100);
-//        long[] numbers = {1L, 2L, 3L, 4L, 5L, 6L};
-        System.out.println("Xor sum: " + NotBlockingFunctionsUtils.xorArray(ArrayUtils.getRandomArrayInteger(10)));
+        long[] numbers = ArrayUtils.getRandomArrayLong(DEFAULT_ARRAY_SIZE);
+        System.out.println("Xor sum: " + NotBlockingFunctionsUtils.xorArray(ArrayUtils.getRandomArrayInteger(DEFAULT_ARRAY_SIZE)));
         System.out.println("Max element: " + NotBlockingFunctionsUtils.maxElement(numbers));
         System.out.println("Min element: " + NotBlockingFunctionsUtils.minElement(numbers));
         System.out.println("Mode: " + NotBlockingFunctionsUtils.mode(numbers));
         System.out.println("Median: " + NotBlockingFunctionsUtils.median(numbers));
-        System.out.println("Scalar product: " + NotBlockingFunctionsUtils.scalarProduct(Arrays.asList(ArrayUtils.getRandomArrayInteger(100000)), Arrays.asList(ArrayUtils.getRandomArrayInteger(100000))));
+        System.out.println("Scalar product: " + NotBlockingFunctionsUtils.scalarProduct(Arrays.asList(ArrayUtils.getRandomArrayInteger(DEFAULT_ARRAY_SIZE)),
+                                                    Arrays.asList(ArrayUtils.getRandomArrayInteger(DEFAULT_ARRAY_SIZE))));
     }
 
 }
